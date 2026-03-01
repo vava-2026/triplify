e3fcf9fdd85ff8dc7191408392b763b4d9fe46bd
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2026-03-02 00:22:08 +0100

    feat: add logging to the categories query handler

--- application/src/main/java/com/triplify/application/category/query/GetAllCategoriesQueryHandler.java
+++ application/src/main/java/com/triplify/application/category/query/GetAllCategoriesQueryHandler.java
@@ -3,11 +3,14 @@ package com.triplify.application.category.query;
 import an.awesome.pipelinr.Command;
 import com.triplify.domain.repository.CategoryRepository;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class GetAllCategoriesQueryHandler implements Command.Handler<GetAllCategoriesQuery, List<CategoryResponse>> {
 
+    private static final Logger log = LoggerFactory.getLogger(GetAllCategoriesQueryHandler.class);
     private final CategoryRepository categoryRepository;
 
     @Inject
@@ -17,6 +20,7 @@ public class GetAllCategoriesQueryHandler implements Command.Handler<GetAllCateg
 
     @Override
     public List<CategoryResponse> handle(GetAllCategoriesQuery query) {
+        log.debug("Getting all categories");
         return categoryRepository.findAll().stream()
                 .map(c -> new CategoryResponse(c.id(), c.name()))
                 .toList();
