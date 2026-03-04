617a781f1a745a20193aa788a229c1bb6509dc30
VolodymyrSorochynsky <159086929+VolodymyrSorochynsky@users.noreply.github.com>
2026-03-05 00:09:01 +0100

    feat: add responsive design on menu and header

--- ui/src/main/java/com/triplify/ui/shared/menu/view/MenuView.java
+++ ui/src/main/java/com/triplify/ui/shared/menu/view/MenuView.java
@@ -9,8 +9,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
@@ -22,23 +20,23 @@ import java.util.ResourceBundle;
 public class MenuView implements Initializable {
 
     private static final double SIDEBAR_WIDTH = 260;
+    private static final double SIDEBAR_COLLAPSED_WIDTH = 0;
 
     @FXML private StackPane sidebarRoot;
     @FXML private VBox mainPageInner;
-    @FXML private Pane collapsedIsland;
     @FXML private VBox navContainer;
-
-    @FXML private SidebarIslandView expandedIslandController;
-    @FXML private SidebarIslandView collapsedIslandInnerController;
 
     @FXML private Label accountRole;
 
     private final MenuViewModel viewModel = new MenuViewModel();
     private final List<NavButtonView> navButtons = new ArrayList<>();
 
+    private SidebarIslandView islandController;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        sidebarRoot.setMaxHeight(Double.MAX_VALUE);
         mainPageInner.setMaxHeight(Double.MAX_VALUE);
 
         for (NavItem navItem : NavItem.values()) {
@@ -51,15 +49,18 @@ public class MenuView implements Initializable {
         accountRole.textProperty().bind(
                 Bindings.createStringBinding(() -> I18n.t("account.role"), I18n.bundleProperty()));
 
-        expandedIslandController.setOnToggle(viewModel::toggleCollapsed);
-        collapsedIslandInnerController.setOnToggle(viewModel::toggleCollapsed);
-
         viewModel.selectedItemProperty().addListener(
                 (obs, oldVal, newVal) -> refreshActiveState(newVal));
         refreshActiveState(viewModel.getSelectedItem());
 
         viewModel.collapsedProperty().addListener(
                 (obs, oldVal, newVal) -> applyCollapsedState(newVal));
+        applyCollapsedState(viewModel.isCollapsed());
+    }
+
+    public void setIslandController(SidebarIslandView island) {
+        this.islandController = island;
+        island.setOnToggle(viewModel::toggleCollapsed);
         applyCollapsedState(viewModel.isCollapsed());
     }
 
@@ -74,23 +75,18 @@ public class MenuView implements Initializable {
     private void refreshActiveState(MenuItem active) {
         navButtons.forEach(btn ->
                 btn.setActive(btn.getNavItem().getMenuItem() == active));
-
-        if (viewModel.isCollapsed()) {
-            applyCollapsedState(true);
-        }
     }
 
     private void applyCollapsedState(boolean collapsed) {
         mainPageInner.setVisible(!collapsed);
         mainPageInner.setManaged(!collapsed);
-        collapsedIsland.setVisible(collapsed);
-        collapsedIsland.setManaged(collapsed);
 
-        sidebarRoot.setPrefWidth(SIDEBAR_WIDTH);
-        sidebarRoot.setMaxWidth(SIDEBAR_WIDTH);
+        double width = collapsed ? SIDEBAR_COLLAPSED_WIDTH : SIDEBAR_WIDTH;
+        sidebarRoot.setPrefWidth(width);
+        sidebarRoot.setMaxWidth(width);
 
-        boolean isMap = viewModel.isHideHeader();
-        expandedIslandController.setCollapsed(collapsed, isMap);
-        collapsedIslandInnerController.setCollapsed(collapsed, isMap);
+        if (islandController != null) {
+            islandController.setCollapsed(collapsed, viewModel.isHideHeader());
+        }
     }
 }
