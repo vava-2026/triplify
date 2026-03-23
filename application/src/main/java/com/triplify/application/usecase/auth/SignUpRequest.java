e2793058cfa55f4942fec726c55d8dc08d0ae5e4
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2026-03-23 22:19:49 +0100

    feat: add sign up request

---
+++ application/src/main/java/com/triplify/application/usecase/auth/SignUpRequest.java
@@ -0,0 +1,33 @@
+package com.triplify.application.usecase.auth;
+
+import com.triplify.application.error.ValidationMessage;
+import com.triplify.domain.model.enums.RoleEnum;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignUpRequest(
+    @NotBlank(message = ValidationMessage.Constants.REQUIRED)
+    @Size(min = 3, message = ValidationMessage.Constants.USERNAME_TOO_SHORT)
+    String username,
+
+    @NotBlank(message = ValidationMessage.Constants.REQUIRED)
+    @Email(message = ValidationMessage.Constants.EMAIL_INVALID)
+    String email,
+
+    @NotBlank(message = ValidationMessage.Constants.REQUIRED)
+    @Size(min = 8, message = ValidationMessage.Constants.PASSWORD_TOO_SHORT)
+    String password,
+
+    @NotBlank(message = ValidationMessage.Constants.REQUIRED)
+    RoleEnum role
+) {
+    @AssertTrue(message = ValidationMessage.Constants.SIGN_UP_INVALID_ROLE)
+    public boolean isValidRole() {
+        if (role == null) {
+            return true;
+        }
+        return role == RoleEnum.CONFIGURATION_MANAGER || role == RoleEnum.USER;
+    }
+}
