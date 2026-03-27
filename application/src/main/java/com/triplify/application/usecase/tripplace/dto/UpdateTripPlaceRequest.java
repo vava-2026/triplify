50c666c5e484bce1356940eb27cec0e3b71d355d
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-03-27 22:28:23 +0100

    refactor: remove unnecessary fields from UpdateTripPlaceRequest DTO

--- application/src/main/java/com/triplify/application/usecase/tripplace/dto/UpdateTripPlaceRequest.java
+++ application/src/main/java/com/triplify/application/usecase/tripplace/dto/UpdateTripPlaceRequest.java
@@ -1,12 +1,9 @@
 package com.triplify.application.usecase.tripplace.dto;
 
 import com.triplify.application.error.ValidationMessage;
-import com.triplify.domain.model.enums.StatusEnum;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.time.Instant;
-import java.util.Set;
 
 public record UpdateTripPlaceRequest(
 
