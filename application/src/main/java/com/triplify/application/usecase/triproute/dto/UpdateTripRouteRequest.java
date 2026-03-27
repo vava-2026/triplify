eb8cc39d6bcf58e225727dea42724e68c924bad7
Yurii Oliinyk <oliinykyurii123@gmail.com>
2026-03-27 22:29:25 +0100

    refactor: remove unnecessary fields from UpdateTripRouteRequest DTO

--- application/src/main/java/com/triplify/application/usecase/triproute/dto/UpdateTripRouteRequest.java
+++ application/src/main/java/com/triplify/application/usecase/triproute/dto/UpdateTripRouteRequest.java
@@ -4,18 +4,10 @@ import com.triplify.application.error.ValidationMessage;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
-import java.util.Set;
-
 public record UpdateTripRouteRequest(
 
         @NotBlank(message = ValidationMessage.Constants.REQUIRED)
         String id,
-
-        @NotBlank(message = ValidationMessage.Constants.REQUIRED)
-        String tripId,
-
-        @NotBlank(message = ValidationMessage.Constants.REQUIRED)
-        String routeId,
 
         @Min(value = 0, message = ValidationMessage.Constants.NUMBER_MUST_BE_NON_NEGATIVE)
         int order
