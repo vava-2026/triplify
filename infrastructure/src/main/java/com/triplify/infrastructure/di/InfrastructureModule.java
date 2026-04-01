2cb586e648416e7d935d64e990f4e1b94f8fe944
PureTL <ananchenko.maks07@gmail.com>
2026-04-01 17:30:50 +0200

    feat: implemented DiscImageSaver, ImageService and ImageRepository

--- infrastructure/src/main/java/com/triplify/infrastructure/di/InfrastructureModule.java
+++ infrastructure/src/main/java/com/triplify/infrastructure/di/InfrastructureModule.java
@@ -2,12 +2,16 @@ package com.triplify.infrastructure.di;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.triplify.domain.repository.ImageRepository;
+import com.triplify.domain.service.ImageStorageService;
 import com.triplify.domain.service.PasswordEncoder;
 import com.triplify.domain.repository.CategoryRepository;
 import com.triplify.domain.repository.UserRepository;
 import com.triplify.infrastructure.repository.CategoryRepositoryImpl;
+import com.triplify.infrastructure.repository.ImageRepositoryImpl;
 import com.triplify.infrastructure.repository.UserRepositoryImpl;
 import com.triplify.infrastructure.security.BCryptPasswordEncoder;
+import com.triplify.infrastructure.storage.DiscImageStorage;
 
 public class InfrastructureModule extends AbstractModule {
 
@@ -15,7 +19,9 @@ public class InfrastructureModule extends AbstractModule {
     protected void configure() {
         bind(CategoryRepository.class).to(CategoryRepositoryImpl.class).in(Singleton.class);
         bind(UserRepository.class).to(UserRepositoryImpl.class).in(Singleton.class);
+        bind(ImageRepository.class).to(ImageRepositoryImpl.class).in(Singleton.class);
         bind(PasswordEncoder.class).to(BCryptPasswordEncoder.class).in(Singleton.class);
+        bind(ImageStorageService.class).to(DiscImageStorage.class).in(Singleton.class);
     }
 }
 
