9146b40d46c0820c5bc49637a0aa8f13a0330f45
VolodymyrSorochynsky <159086929+VolodymyrSorochynsky@users.noreply.github.com>
2026-04-05 04:06:33 +0200

    fix: merge fix

--- ui/src/main/java/com/triplify/ui/MainApp.java
+++ ui/src/main/java/com/triplify/ui/MainApp.java
@@ -199,37 +199,6 @@ public class MainApp extends Application {
         if (themeUrl == null) throw new IllegalStateException("theme.css not found");
         scene.getStylesheets().add(themeUrl.toExternalForm());
 
-        // TODO: remove, only for testing
-        authService.login(new LogInRequest("admin@triplify.com", "password"));
-
-        var austriaPage = new PageRequest(0, 10);
-        var austriaFilter = new CountryFilter("Austria", null, false);
-        var austria = countryService.getCountries(new GetCountriesRequest(austriaPage, austriaFilter)).getValue().items().getFirst();
-
-        var vienna = placeService.addPlace(new AddPlaceRequest(austria.id(), null, "Vienna", "Capital of Austria", 48.2082, 16.3738)).getValue();
-        var linz = placeService.addPlace(new AddPlaceRequest(austria.id(), null, "Linz", "City in Austria", 4.2, 18.3738)).getValue();
-        var grazRes = placeService.addPlace(new AddPlaceRequest(austria.id(), null, "Graz", "City in Austria", 88.22, 13.38));
-        var graz = grazRes.getValue();
-
-        var viennaUpdated = placeService.updatePlace(new UpdatePlaceRequest(vienna.id(), austria.id(), null, "Vienna Updated", "Capital of Austria, updated", 48.2082, 16.3738)).getValue();
-        assert(viennaUpdated.id().equals(vienna.id()));
-
-         var placesInAustria = placeService.getPlaces(new GetPlacesRequest(new PageRequest(0, 10), new PlaceFilter(null, austria.id()))).getValue().items();
-         assert(placesInAustria.size() == 3);
-
-         placeService.deletePlace(new DeletePlaceRequest(linz.id()));
-         var placesAfterDeletion = placeService.getPlaces(new GetPlacesRequest(new PageRequest(0, 10), new PlaceFilter(null, null))).getValue().items();
-         assert(placesAfterDeletion.size() == 2);
-         assert(placesAfterDeletion.stream().noneMatch(p -> p.id().equals(linz.id())));
-
-         var grazFromDb = placeService.getPlaces(new GetPlacesRequest(new PageRequest(0, 10), new PlaceFilter("gra", null))).getValue().items().getFirst();
-         assert(grazFromDb.id().equals(graz.id()));
-
-        placeService.deletePlace(new DeletePlaceRequest(graz.id()));
-        placeService.deletePlace(new DeletePlaceRequest(vienna.id()));
-
-         log.info("Test data setup complete");
-
         stage.setTitle("Triplify");
         stage.setScene(scene);
         stage.show();
