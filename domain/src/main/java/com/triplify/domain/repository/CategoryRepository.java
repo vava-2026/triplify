e185a68218874f8730dc1f565eccda48637eb3fb
Max Ananchenko <152657108+PureTeamLead@users.noreply.github.com>
2026-04-08 20:11:13 +0200

    feat: implemented Category repository and service implementations (#38)

--- domain/src/main/java/com/triplify/domain/repository/CategoryRepository.java
+++ domain/src/main/java/com/triplify/domain/repository/CategoryRepository.java
@@ -3,9 +3,14 @@ package com.triplify.domain.repository;
 import com.triplify.domain.model.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository {
     List<Category> findAll();
-    void save(Category category);
+    Optional<Category> findById(String id);
+    boolean existsByName(String name, String nameSk);
+    void create(Category category);
+    void update(Category category);
+    void delete(Category category);
 }
 
