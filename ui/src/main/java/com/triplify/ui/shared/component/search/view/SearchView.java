c9763e5c831c946d35c8aed029bd91bbbdab4553
Andrew Malokhatko <andrew.malokhatko@gmail.com>
2026-04-26 15:12:26 +0200

    fix: run search even if the query is empty in search component

--- ui/src/main/java/com/triplify/ui/shared/component/search/view/SearchView.java
+++ ui/src/main/java/com/triplify/ui/shared/component/search/view/SearchView.java
@@ -223,6 +223,9 @@ public class SearchView<T> extends VBox {
         String normalizedQuery = query == null ? "" : query.trim();
         activeQuery = normalizedQuery;
 
+        // IMPORTANT: always run search even if the query is empty, application logic hardly relies on this
+        List<Entry<T>> results = model.search(normalizedQuery);
+
         if (normalizedQuery.isEmpty() && !model.isShowOnEmptyQuery()) {
             resultsListView.getSelectionModel().clearSelection();
             resultsListView.getItems().clear();
@@ -230,7 +233,6 @@ public class SearchView<T> extends VBox {
             return;
         }
 
-        List<Entry<T>> results = model.search(normalizedQuery);
         resultsListView.getSelectionModel().clearSelection();
         resultsListView.getItems().setAll(results);
         updateListViewHeight();
