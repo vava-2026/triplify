ffcc67825c92fece9ce427a085d73b655d7769e2
George Sladkovsky <102670724+GeorgiyGit@users.noreply.github.com>
2026-05-05 21:01:20 +0200

    feat: restore seeders back

--- infrastructure/src/main/java/com/triplify/infrastructure/repository/persistence/DatabaseMigrationInitializer.java
+++ infrastructure/src/main/java/com/triplify/infrastructure/repository/persistence/DatabaseMigrationInitializer.java
@@ -15,7 +15,7 @@ import com.google.inject.Singleton;
 @Singleton
 public class DatabaseMigrationInitializer {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseMigrationInitializer.class);
-    private static final int CURRENT_SCHEMA_VERSION = 13;
+    private static final int CURRENT_SCHEMA_VERSION = 20;
     private static final List<MigrationStep> MIGRATIONS = List.of(
             new MigrationStep(1, "migrations/initial.sql"),
             new MigrationStep(2, "migrations/V2__countries_created_by_nullable.sql"),
@@ -29,14 +29,14 @@ public class DatabaseMigrationInitializer {
             new MigrationStep(10, "seeders/badge_image_seeder.sql"),
             new MigrationStep(11, "seeders/badge_seeder.sql"),
             new MigrationStep(12, "migrations/V7__create_statistics_table.sql"),
-            new MigrationStep(13, "seeders/emotion_seeder.sql")
-            //new MigrationStep(14, "seeders/user_seeder.sql"),
-            //new MigrationStep(15, "seeders/tag_seeder.sql"),
-            //new MigrationStep(16, "seeders/place_seeder.sql"),
-            //new MigrationStep(17, "seeders/route_seeder.sql"),
-            //new MigrationStep(18, "seeders/trip_seeder.sql"),
-            //new MigrationStep(19, "seeders/story_seeder.sql"),
-            //new MigrationStep(20, "seeders/statistics_seeder.sql")
+            new MigrationStep(13, "seeders/emotion_seeder.sql"),
+            new MigrationStep(14, "seeders/user_seeder.sql"),
+            new MigrationStep(15, "seeders/tag_seeder.sql"),
+            new MigrationStep(16, "seeders/place_seeder.sql"),
+            new MigrationStep(17, "seeders/route_seeder.sql"),
+            new MigrationStep(18, "seeders/trip_seeder.sql"),
+            new MigrationStep(19, "seeders/story_seeder.sql"),
+            new MigrationStep(20, "seeders/statistics_seeder.sql")
     );
 
     public void initialize() {
